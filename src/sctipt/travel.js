7c185fa93f3aa201726d18051a37754b0e0cd065
kenton116 <57275744+kenton116@users.noreply.github.com>
2021-08-01 18:30:44 +0900

    Update travel.js

--- src/sctipt/travel.js
+++ src/sctipt/travel.js
@@ -13,16 +13,4 @@ const country = [
     'Singapore',
     'Spain',
     'UK',
-]
-
-
-// img要素を作成
-let img_element = document.createElement('img');
-img_element.src = 'media_src/country/'+ country[0]; // 画像パス
-img_element.alt = country[0] + 'の地図画像'; // 代替テキスト
-img_element.width = 200; // 横サイズ（px）
-img_element.height = 200; // 縦サイズ（px）
-
-// 指定した要素にimg要素を挿入
-const content_area = document.getElementById("content_area");
-content_area.appendChild(img_element);
+];
